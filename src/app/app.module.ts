53cd79f5ef77c0249a2fa70d7e6f9f39c87845d7
Lukas Knoepfel <shylux@gmail.com>
2017-12-19 17:14:10 +0100

    Added details and home compoents.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { CatalogComponent } from './catalog/catalog.component';
 import {FormsModule} from "@angular/forms";
 import {BookComponent} from "./book/book.component";
+import { CustomerDetailsComponent } from './customer-details/customer-details.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     CatalogComponent,
-    BookComponent
+    BookComponent,
+    CustomerDetailsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule, FormsModule
---
+++ src/app/customer-details/customer-details.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-customer-details',
+  template: `
+    <p>
+      customer-details works!
+    </p>
+  `,
+  styles: []
+})
+export class CustomerDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
